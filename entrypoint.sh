171b70a045fbde376f28307f88558de3624c7789
Felix Niederer <felix.niederer@adfinis.com>
2025-10-10 11:58:02 +0200

    feat: Updated docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-
 python /app/version_checker.py \
   --token="${INPUT_TOKEN}" \
   --charts-path="${INPUT_CHARTS_PATH}" \
-  --maintainers-file="${INPUT_MAINTAINERS_FILE}" \
+  --maintainers-file="${INPUT_MAINTAINERS_FILE}"
