26121d2e3e865807de49b982e6967252dd82ccf3
Felix Niederer <felix.niederer@adfinis.com>
2025-10-10 11:36:26 +0200

    fix: removed migration artifact in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 
-python /app/scripts/version_checker.py \
+python /app/version_checker.py \
   --token="${INPUT_TOKEN}" \
   --charts-path="${INPUT_CHARTS_PATH}" \
   --maintainers-file="${INPUT_MAINTAINERS_FILE}" \
