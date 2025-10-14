4bf20194accbdac7d489b88ec957c10de3dc21e8
Felix Niederer <felix.niederer@adfinis.com>
2025-10-14 11:57:22 +0200

    moved source

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-python /app/version_checker.py \
+python /app/src/version_checker.py \
   --token="${INPUT_TOKEN}" \
   --charts-path="${INPUT_CHARTS_PATH}" \
   --maintainers-file="${INPUT_MAINTAINERS_FILE}"
